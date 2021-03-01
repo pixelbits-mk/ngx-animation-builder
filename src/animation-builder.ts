5ae7c21c267d1bca09abd75afe1db0a22073549e
kmike <mike@joinsherpa.com>
2021-03-01 01:12:36 -0500

    updated

--- src/animation-builder.ts
+++ src/animation-builder.ts
@@ -1,25 +1,41 @@
-import { animate, keyframes, state, style, transition, trigger } from "@angular/animations"
+import {
+  animate,
+  keyframes,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
 
 export interface AnimationState {
-    [state: string]: any
+  [state: string]: any;
 }
 export interface AnimationTransition {
-    expression: string,
-    animate: string,
-    keyframes?: any[]
+  expression: string;
+  animate: string;
+  keyframes?: any[];
 }
 export interface TriggerDefinition {
-    states?: AnimationState,
-    transitions?: AnimationTransition[]
+  states?: AnimationState;
+  transitions?: AnimationTransition[];
 }
 export interface AnimationTrigger {
-    [trigger: string]: TriggerDefinition
+  [trigger: string]: TriggerDefinition;
 }
 export function animationBuilder(animation: AnimationTrigger) {
-    return Object.keys(animation).map(t => {
-        return trigger(t, [
-            ...Object.keys(animation[t].states).map(x => state(x, style(animation[t].states[x]))),
-            ...animation[t].transitions.map(x => transition(x.expression, animate(x.animate, keyframes((x?.keyframes)?.map(z => style(z))))))
-        ])
-    })
+  return Object.keys(animation).map((t) => {
+    return trigger(t, [
+      ...Object.keys(animation[t].states).map((x) =>
+        state(x, style(animation[t].states[x]))
+      ),
+      ...animation[t].transitions.map((x) =>
+        x.keyframes
+          ? transition(
+              x.expression,
+              animate(x.animate, keyframes(x.keyframes.map((z) => style(z))))
+            )
+          : transition(x.expression, animate(x.animate))
+      ),
+    ]);
+  });
 }
